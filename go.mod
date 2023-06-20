c70265b4d09671cb49d24211b9a9cd723af245f6
Pooyan Razian <prazian@gmail.com>
2023-06-20 14:24:55 +0200

    Remove html tags from slag message

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/digi-wolk/rss-feed-reader
 go 1.20
 
 require (
-	github.com/gorilla/websocket v1.5.0 // indirect
-	github.com/slack-go/slack v0.12.2 // indirect
+	github.com/slack-go/slack v0.12.2
+	golang.org/x/net v0.11.0
 )
+
+require github.com/gorilla/websocket v1.4.2 // indirect
--- internal/integrations/slack.go
+++ internal/integrations/slack.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"github.com/digi-wolk/rss-feed-reader/internal/rss"
 	"github.com/slack-go/slack"
+	"golang.org/x/net/html"
 	"log"
 	"os"
+	"strings"
 )
 
 func CommentOnSlack(items []rss.Item, slackChannel string) {
@@ -27,8 +29,10 @@ func CommentOnSlack(items []rss.Item, slackChannel string) {
 }
 
 func buildMessageBlocks(item rss.Item) []slack.Block {
+	description := removeHTMLTags(item.Description)
+
 	titleBlock := slack.NewTextBlockObject("mrkdwn", fmt.Sprintf("*Title:*\n%s", item.Title), false, false)
-	descriptionBlock := slack.NewTextBlockObject("mrkdwn", fmt.Sprintf("*Description:*\n%s", item.Description), false, false)
+	descriptionBlock := slack.NewTextBlockObject("mrkdwn", fmt.Sprintf("*Description:*\n%s", description), false, false)
 	linkBlock := slack.NewTextBlockObject("mrkdwn", fmt.Sprintf("*Link:*\n%s", item.Link), false, false)
 	pubDateBlock := slack.NewTextBlockObject("mrkdwn", fmt.Sprintf("*PubDate:*\n%s", item.PubDate), false, false)
 
@@ -38,4 +42,26 @@ func buildMessageBlocks(item rss.Item) []slack.Block {
 		slack.NewSectionBlock(linkBlock, nil, nil),
 		slack.NewSectionBlock(pubDateBlock, nil, nil),
 	}
+}
+
+func removeHTMLTags(htmlContent string) string {
+	doc, err := html.Parse(strings.NewReader(htmlContent))
+	if err != nil {
+		log.Printf("Error parsing HTML: %s", err.Error())
+		return htmlContent // Return original content as fallback
+	}
+
+	var textContent string
+	var extractText func(*html.Node)
+	extractText = func(n *html.Node) {
+		if n.Type == html.TextNode {
+			textContent += n.Data
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			extractText(c)
+		}
+	}
+
+	extractText(doc)
+	return strings.TrimSpace(textContent)
 }
