b85f4e23df2149afad4bd2b2dda7e7227f032103
Pooyan Razian <prazian@gmail.com>
2023-06-20 15:20:37 +0200

    Rename to rssread

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,7 +2,7 @@
 # This file was inspired by gosec project
 
 # Binary name based on the value specified in Dockerfile
-BIN_NAME=readrss
+BIN_NAME=rssread
 
 # Expand the arguments into an array of strings. This is required because the GitHub action
 # provides all arguments concatenated as a single string.
