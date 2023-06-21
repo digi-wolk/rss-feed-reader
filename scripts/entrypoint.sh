ece5f7d55d72ea4e13566e2522d2cfdec29d37b8
Pooyan Razian <prazian@gmail.com>
2023-06-21 11:52:09 +0200

    Pass arguments regular way

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
-# This file was inspired by gosec project
-
 # Binary name based on the value specified in Dockerfile
 BIN_NAME=rssread
 
-# Expand the arguments into an array of strings. This is required because the GitHub action
-# provides all arguments concatenated as a single string.
-ARGS=("$@")
-
-/bin/${BIN_NAME} "${ARGS[*]}"
+/bin/${BIN_NAME} "$@"
